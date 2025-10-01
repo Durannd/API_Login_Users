bcf4916816c471adb6b131608cf5eb7ba4a9e004
Ricael <ricaelmenezes@gmail.com>
2025-10-01 11:17:28 -0300

    allow cors

--- src/main/resources/WebConfig_Version2.java
+++ src/main/resources/WebConfig_Version2.java
@@ -9,8 +9,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("https://seu-front.exemplo.com") // coloque o domínio do seu front
-            .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+            .allowedOrigins("*") // coloque o domínio do seu front
+            .allowedMethods("GET","POST","PUT","DELETE","OPTIONS","PATCH")
             .allowedHeaders("*")
             .allowCredentials(true);
     }
